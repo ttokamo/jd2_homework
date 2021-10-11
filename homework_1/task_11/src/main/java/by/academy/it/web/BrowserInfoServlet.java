494c2c32768040e3846f6f7fda809971d5b2d5e3
tokamo <worcers@gmail.com>
2021-10-11 21:54:16 +0300

    feat: task_11

---
+++ homework_1/task_11/src/main/java/by/academy/it/web/BrowserInfoServlet.java
@@ -0,0 +1,35 @@
+package by.academy.it.web;
+
+import eu.bitwalker.useragentutils.Browser;
+import eu.bitwalker.useragentutils.UserAgent;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "browserInfoServlet", urlPatterns = "/info")
+public class BrowserInfoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            PrintWriter out = resp.getWriter();
+            UserAgent userAgent = UserAgent.parseUserAgentString(req.getHeader("User-Agent"));
+            Browser browser = userAgent.getBrowser();
+            String browserName = browser.getName();
+
+            out.println("<html lang=ru><center><meta charset=UTF-8 /><h1>Приветсвую пользователя "
+                    + browserName
+                    + "</h1></center></html>");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        doGet(req, resp);
+    }
+}
