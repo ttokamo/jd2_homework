64a71560d43e9d4e411d46f82c5e0698a878baf9
tokamo <worcers@gmail.com>
2021-10-11 23:20:08 +0300

    update: task_12

--- homework_1/task_12/src/main/java/by/academy/it/web/NumberOfVisitorsV2Servlet.java
+++ homework_1/task_12/src/main/java/by/academy/it/web/NumberOfVisitorsV2Servlet.java
@@ -14,27 +14,27 @@ public class NumberOfVisitorsV2Servlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        resp.setContentType("image/jpeg");
-        BufferedImage image = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics = image.createGraphics();
-        graphics.setFont(new Font("Serif", Font.ITALIC, 30));
-        graphics.setColor(Color.GREEN);
-        path = getServletContext().getInitParameter("visitorsLogV2");
-        int count;
-
-        if (readNumberOfVisitorsFromFile() != null) {
-            count = Integer.parseInt(readNumberOfVisitorsFromFile());
-        } else {
-            count = 0;
-        }
-        count++;
-        writeNumberOfVisitorsToFile(count);
-        graphics.drawString("Number of visitors: " + count, 300, 300);
         try {
+            resp.setContentType("image/jpeg");
+            BufferedImage image = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics = image.createGraphics();
+            graphics.setFont(new Font("Serif", Font.ITALIC, 30));
+            graphics.setColor(Color.GREEN);
+            path = getServletContext().getInitParameter("visitorsLogV2");
+            int count;
+
+            if (readNumberOfVisitorsFromFile() != null) {
+                count = Integer.parseInt(readNumberOfVisitorsFromFile());
+            } else {
+                count = 0;
+            }
+            count++;
+            writeNumberOfVisitorsToFile(count);
+            graphics.drawString("Number of visitors: " + count, 300, 300);
             ServletOutputStream out = resp.getOutputStream();
             ImageIO.write(image, "jpeg", out);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
